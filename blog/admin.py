c170c2c89beaa728f3ea01900216ff4ec9c6abda
alaaedinne <73131118+alaaedinne@users.noreply.github.com>
2021-01-13 18:29:09 +0100

    Add files via upload

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import job , category
+
+
+admin.site.register(job)
+admin.site.register(category)
---
+++ home/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Home
+
+
+admin.site.register(Home)
